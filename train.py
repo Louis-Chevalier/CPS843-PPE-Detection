6bb85e00d748e5450faaed6d7ede144b26b02d0d
Louis <louis.chevalier@torontmu.ca>
2025-12-01 16:18:39 -0500

    added command line arguments for train.py

--- train.py
+++ train.py
@@ -1,6 +1,19 @@
 from ultralytics import YOLO
+import sys
 
-model = YOLO("yolo11m.pt")
+model_name ='yolo_v11x_V01'
+model_type = "yolo11m.pt"
+
+# python3 train.py model_name path/to/model_type
+if len(sys.argv) == 3:
+    model_type = sys.argv[2]
+    model_name = sys.argv[1]
+    print(f'Beginning training of {model_name} using {model_type}')
+elif len(sys.argv) != 1 or len(sys.argv) !=3:
+    print("Invalid amount of arguments")
+    sys.exit()
+
+model = YOLO(model_type)
 
 model.train(
     data='./data.yaml', 
@@ -9,8 +22,8 @@ model.train(
     imgsz=640,
     device='cuda:0',
     #device='cpu',
-    name='yolo_v11x_V01',
-    project='trained_models/yolo_v1_V01',
+    name=model_name,
+    project='trained_models/'+model_name,
     save_period=5,
     amp=False,
     workers=0,
